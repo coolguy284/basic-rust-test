05e3281bb166c7c67827645246e519a86cc8bbb1
coolguy284 <coolguy284v64@gmail.com>
2023-07-22 21:42:49 -0400

    add proper time zone abbreviation to current_time command
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
-use chrono::{Utc, Local, TimeZone, DateTime, Offset};
+use chrono::{DateTime, Local, Offset, TimeZone, Utc};
+use chrono_tz::{OffsetName, Tz};
+use iana_time_zone::get_timezone;
 
 fn main() {
   let cmd_line_args: Vec<String> = env::args().collect();
@@ -24,23 +26,26 @@ fn main() {
         let now_local: DateTime<Local> = DateTime::from(now_utc);
         
         // i don't even know bro https://stackoverflow.com/questions/59603665/how-do-you-find-the-local-timezone-offset-in-rust/59603899#59603899
-        let local_offset = Local.timestamp_opt(0, 0).single().unwrap().offset().fix().local_minus_utc();
+        let local_offset_secs = Local.timestamp_opt(0, 0).single().unwrap().offset().fix().local_minus_utc();
         
-        let local_offset_is_positive = local_offset >= 0;
-        let local_offset_absolute = local_offset.abs();
+        let local_offset_secs_is_positive = local_offset_secs >= 0;
+        let local_offset_secs_absolute = local_offset_secs.abs();
         
-        let local_tz_str = iana_time_zone::get_timezone().unwrap();
+        let local_tz_str = get_timezone().unwrap();
+        let local_tz: Tz = local_tz_str.parse().unwrap();
+        let local_offset = local_tz.offset_from_utc_date(&now_utc.date_naive());
+        let local_tz_abbreviation = local_offset.abbreviation();
         
         println!("Current time, written in many ways:");
         println!();
         println!("Human readable:");
-        println!("  Current Local time is {} {}", now_local.format("%A, %B %d, %Y, %I:%M:%S.%9f %p %z"), local_tz_str);
+        println!("  Current Local time is {} {}", now_local.format("%A, %B %d, %Y, %I:%M:%S.%9f %p %z"), local_tz_abbreviation);
         println!("  Current UTC time is   {}", now_utc.format("%A, %B %d, %Y, %I:%M:%S.%9f %p %z %Z"));
         println!();
         println!("Technical:");
         println!("  Current Local time is {}", now_local.format("%Y-%m-%dT%H:%M:%S.%9f"));
         println!("  Current UTC time is   {}", now_utc.format("%Y-%m-%dT%H:%M:%S.%9fZ"));
-        println!("  Local time offset is  {}{}:{:0>2} ({})", if local_offset_is_positive { "" } else { "-" }, local_offset_absolute / 3600, local_offset_absolute / 60 % 60, local_tz_str);
+        println!("  Local time offset is  {}{}:{:0>2} ({}, {})", if local_offset_secs_is_positive { "" } else { "-" }, local_offset_secs_absolute / 3600, local_offset_secs_absolute / 60 % 60, local_tz_abbreviation, local_tz_str);
       },
       _ => {
         println!("Invalid argument {}, for list of arguments run \"basic-rust-test\" with no arguments", arg_1);
