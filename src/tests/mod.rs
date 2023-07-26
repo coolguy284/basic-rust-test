d2745aa9e7bb394f92c52a19bacf4ede77032f35
coolguy284 <coolguy284v64@gmail.com>
2023-07-26 04:31:17 -0400

    split tests into many files
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

---
+++ src/tests/mod.rs
@@ -0,0 +1,6 @@
+// testing others' code (due to weird bug that ended up being not actually the fault of others' code but leaving the tests in here anyway)
+pub mod sha3;
+
+// testing my own code
+pub mod cgcsprng;
+pub mod mt19937;
--- src/tests/sha3.rs
+++ src/tests/sha3.rs
@@ -2,61 +2,6 @@ use crypto::digest::Digest;
 use crypto::sha3::Sha3;
 use hex::FromHex;
 
-use crate::cgrandom::engine::cgcsprng1::CgCsPrng1;
-use crate::cgrandom::engine::engine::RngEngine;
-use crate::cgrandom::engine::mt19937::{Mt19937_32, Mt19937_64};
-
-// the 10000th output of mt19937_32 seeded with 5489 must be 4123659995
-#[test]
-fn test_mt19937_32() {
-  let mut rng = Mt19937_32::new();
-  
-  rng.seed(5489u32);
-  
-  for _ in 1usize..=9999usize {
-    rng.generate();
-  }
-  
-  let result = rng.generate();
-  
-  assert_eq!(result, 4123659995u32);
-}
-
-// the 10000th output of mt19937_64 seeded with 5489 must be 9981545732273789042
-#[test]
-fn test_mt19937_64() {
-  let mut rng = Mt19937_64::new();
-  
-  rng.seed(5489u64);
-  
-  for _ in 1usize..=9999usize {
-    rng.generate();
-  }
-  
-  let result = rng.generate();
-  
-  assert_eq!(result, 9981545732273789042u64);
-}
-
-// the 10000th output of cgcsprng1 seeded with fe4464b847255caa418f92061e23f8bac922e979a2c323b4bec1b4b5f171faecec3390232216fb90ffbc4f20cc75902f91a52d4d7fae0068b7e2498ae01db135 and with modifier 9ff1fd69c060fc029f6b30566f2fb763d939f519187a38a528caed3d60e60c9ef9f1b51f1dd0c7ee4e9265f38919e09f38511f9f21f0779ddb290084005b7f3a must be ee40bb060265620041ceef6f69d7f1c5f968d936e8750e249fc0b50c3744c356658dcc877c25450d27f73971e2468396f63d10ba62dd56f8cd9d1060fe5b9618
-// (it is sha3-512 hash of hex "fe4464b847255caa418f92061e23f8bac922e979a2c323b4bec1b4b5f171faecec3390232216fb90ffbc4f20cc75902f91a52d4d7fae0068b7e2498ae01db1359ff1fd69c060fc029f6b30566f2fb763d939f519187a38a528caed3d60e60c9ef9f1b51f1dd0c7ee4e9265f38919e09f38511f9f21f0779ddb290084005b7f3a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000270f", all 3 concatenated together)
-#[test]
-fn test_cgcsprng1() {
-  let rng_seed = <[u8; 64]>::from_hex("fe4464b847255caa418f92061e23f8bac922e979a2c323b4bec1b4b5f171faecec3390232216fb90ffbc4f20cc75902f91a52d4d7fae0068b7e2498ae01db135").expect("this test has invalid hex seed somehow");
-  let rng_modifier = <[u8; 64]>::from_hex("9ff1fd69c060fc029f6b30566f2fb763d939f519187a38a528caed3d60e60c9ef9f1b51f1dd0c7ee4e9265f38919e09f38511f9f21f0779ddb290084005b7f3a").expect("this test has invalid hex modifier somehow");
-  
-  let mut rng = CgCsPrng1::new();
-  
-  rng.seed(rng_seed);
-  rng.set_modifier(rng_modifier);
-  
-  rng.skip(9999);
-  
-  let result = rng.generate();
-  
-  assert_eq!(result, <[u8; 64]>::from_hex("ee40bb060265620041ceef6f69d7f1c5f968d936e8750e249fc0b50c3744c356658dcc877c25450d27f73971e2468396f63d10ba62dd56f8cd9d1060fe5b9618").expect("this test has invalid hex result somehow"));
-}
-
 // sha3-512 hash of "" should be "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26"
 #[test]
 fn test_sha3_emptyhash() {
