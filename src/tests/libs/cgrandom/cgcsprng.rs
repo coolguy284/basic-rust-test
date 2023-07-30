3045cbe0ee4f3fe980f452711c8121312364d46c
coolguy284 <coolguy284v64@gmail.com>
2023-07-30 18:42:34 -0400

    finish converting hex strings to hex! macro
    finish converting runtime-evaluated hex strings to compiled-in hex! macro
    
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- src/tests/libs/cgrandom/cgcsprng.rs
+++ src/tests/libs/cgrandom/cgcsprng.rs
@@ -1,4 +1,4 @@
-use hex::FromHex;
+use hex_literal::hex;
 
 use crate::libs::cgrandom::generators::cgcsprng1::CgCsPrng1;
 use crate::libs::cgrandom::generators::generator::{RngBase, RngSkippable};
@@ -7,8 +7,8 @@ use crate::libs::cgrandom::generators::generator::{RngBase, RngSkippable};
 // (it is sha3-512 hash of hex "fe4464b847255caa418f92061e23f8bac922e979a2c323b4bec1b4b5f171faecec3390232216fb90ffbc4f20cc75902f91a52d4d7fae0068b7e2498ae01db1359ff1fd69c060fc029f6b30566f2fb763d939f519187a38a528caed3d60e60c9ef9f1b51f1dd0c7ee4e9265f38919e09f38511f9f21f0779ddb290084005b7f3a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000270f", all 3 concatenated together)
 #[test]
 fn test_cgcsprng1() {
-  let rng_seed = <[u8; 64]>::from_hex("fe4464b847255caa418f92061e23f8bac922e979a2c323b4bec1b4b5f171faecec3390232216fb90ffbc4f20cc75902f91a52d4d7fae0068b7e2498ae01db135").expect("this test has invalid hex seed somehow");
-  let rng_modifier = <[u8; 64]>::from_hex("9ff1fd69c060fc029f6b30566f2fb763d939f519187a38a528caed3d60e60c9ef9f1b51f1dd0c7ee4e9265f38919e09f38511f9f21f0779ddb290084005b7f3a").expect("this test has invalid hex modifier somehow");
+  let rng_seed = hex!("fe4464b847255caa418f92061e23f8bac922e979a2c323b4bec1b4b5f171faecec3390232216fb90ffbc4f20cc75902f91a52d4d7fae0068b7e2498ae01db135");
+  let rng_modifier = hex!("9ff1fd69c060fc029f6b30566f2fb763d939f519187a38a528caed3d60e60c9ef9f1b51f1dd0c7ee4e9265f38919e09f38511f9f21f0779ddb290084005b7f3a");
   
   let mut rng = CgCsPrng1::new();
   
@@ -19,5 +19,5 @@ fn test_cgcsprng1() {
   
   let result = rng.generate();
   
-  assert_eq!(result, <[u8; 64]>::from_hex("ee40bb060265620041ceef6f69d7f1c5f968d936e8750e249fc0b50c3744c356658dcc877c25450d27f73971e2468396f63d10ba62dd56f8cd9d1060fe5b9618").expect("this test has invalid hex result somehow"));
+  assert_eq!(result, hex!("ee40bb060265620041ceef6f69d7f1c5f968d936e8750e249fc0b50c3744c356658dcc877c25450d27f73971e2468396f63d10ba62dd56f8cd9d1060fe5b9618"));
 }
